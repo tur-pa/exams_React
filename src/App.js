24f477f1bec0ac2daf4c45095f45649befea9950
Paweł <pavelturczynski@gmail.com>
2023-01-24 22:25:48 +0100

    added product_list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import ProductsList from './components/ProductsList/ProductsList';
-import ShopingList from './components/ShopingList/ShopingList';
-import AddProducts from './components/AddProducts/AddProducts';
-import ProductsFilters from './components/ProductsFilters/ProductsFilters';
-import styles from './App.module.scss';
+import ProductsList from "./components/ProductsList/ProductsList";
+import ShopingList from "./components/ShopingList/ShopingList";
+import AddProducts from "./components/AddProducts/AddProducts";
+import ProductsFilters from "./components/ProductsFilters/ProductsFilters";
+import produkty from "./common/consts/produkty";
+import styles from "./App.module.scss";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [fullProductsList, setfullProductsList] = useState(produkty);
+  const [filteredProductsList, setFilteredProductsList] =
+    useState(fullProductsList);
+
   return (
     <div className={styles.appWrapper}>
       <AddProducts />
       <ProductsFilters />
       <div className={styles.columnsWrapper}>
-        <ProductsList />
+        <ProductsList newProductsList={filteredProductsList} />
         <ShopingList />
       </div>
-    </ div>
+    </div>
   );
 }
 
